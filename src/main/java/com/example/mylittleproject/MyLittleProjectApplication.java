cc6ff7498a864e005f14322b01a116f0867f548b
yuyujue <yujue_official@163.com>
2024-04-01 16:53:07 +0800

    feat: init this my little project

---
+++ src/main/java/com/example/mylittleproject/MyLittleProjectApplication.java
@@ -0,0 +1,16 @@
+package com.example.mylittleproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyLittleProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyLittleProjectApplication.class, args);
+
+        // write code for hello word
+        System.out.println("Hello World");
+    }
+
+}
---
+++ src/main/resources/db/migration/V202404011632__create_clothes_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE clothes
+(
+    id  VARCHAR(128)  PRIMARY KEY,
+    name  VARCHAR(128) NOT NULL,
+    season  VARCHAR(64),
+    color VARCHAR  (64),
+    thickness  INTEGER
+);
